39d43fc949c78afbdab619fbedc82442e14164c2
ConanZhang <conankzhang@gmail.com>
2018-05-17 02:17:55 -0600

    Add effects to firing

--- Source/CoopGame/Private/SWeapon.cpp
+++ Source/CoopGame/Private/SWeapon.cpp
@@ -15,6 +15,8 @@ ASWeapon::ASWeapon()
 
 	MeshComp = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("MeshComp"));
 	RootComponent = MeshComp;
+
+	MuzzleSocketName = "MuzzleSocket";
 }
 
 // Called when the game starts or when spawned
@@ -22,6 +24,7 @@ void ASWeapon::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	MuzzleSocketName = "MuzzleSocket";
 }
 
 void ASWeapon::Fire()
@@ -50,10 +53,19 @@ void ASWeapon::Fire()
 		{
 			AActor* HitActor = Hit.GetActor();
 			UGameplayStatics::ApplyPointDamage(HitActor, 20.0f, ShotDirection, Hit, MyOwner->GetInstigatorController(), this, DamageType);
+
+			if(ImpactEffect)
+			{
+				UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ImpactEffect, Hit.ImpactPoint, Hit.ImpactNormal.Rotation());
+			}
 		}
 
 		DrawDebugLine(GetWorld(), EyeLocation, TraceEnd, FColor::White, false, 1.0f, 0, 1.0f);
 
+		if(MuzzleEffect)
+		{
+			UGameplayStatics::SpawnEmitterAttached(MuzzleEffect, MeshComp, MuzzleSocketName);
+		}
 	}
 }
 

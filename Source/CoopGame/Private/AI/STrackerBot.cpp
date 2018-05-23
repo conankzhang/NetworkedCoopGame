b1ee97f7bb69335e2a0b5a149665b572eaa97ca8
ConanZhang <conankzhang@gmail.com>
2018-05-23 02:01:40 -0600

    feat(powerup, pickup): set up powerup and pickup classes

--- Source/CoopGame/Private/AI/STrackerBot.cpp
+++ Source/CoopGame/Private/AI/STrackerBot.cpp
@@ -186,6 +186,7 @@ void ASTrackerBot::Tick(float DeltaTime)
 
 void ASTrackerBot::NotifyActorBeginOverlap(AActor* OtherActor)
 {
+	Super::NotifyActorBeginOverlap(OtherActor);
 	if (!bStartedSelfDestruction && !bExploded)
 	{
 		ASCharacter* PlayerPawn = Cast<ASCharacter>(OtherActor);
